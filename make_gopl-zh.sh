6e1bf362e4edb1e9f88e08e31f87c458913bfe0e
root <root@ip-172-31-25-181.ap-southeast-1.compute.internal>
2022-06-04 00:01:01 +0800

    新增一键安装docker

--- make_gopl-zh.sh
+++ make_gopl-zh.sh
@@ -1,13 +1,70 @@
 #!/bin/bash
 
 BASEDIR=$(cd $(dirname $0)/; pwd -P)
-GOPL_DIR=${BASEDIR}/gopl-zh/gopl-zh
+GOPL_DIR=${BASEDIR}/gopl-zh
 GOPL_DIR_TMP=/tmp/gopl-zh
 GOPL_GIT=https://github.com/gopl-zh/gopl-zh.github.com.git
 
 source /etc/profile
 export PATH=$PATH:/usr/local/go/bin
 
+echo
+type -a jq 2>/dev/null 1>&2
+if [[ $? = 0 ]]; then
+  echo "[Info] jq exists, the version is:"
+  jq --version
+else
+  echo "[Info] jq doesn't exist, install it now."
+  yum -y install jq
+  [[ $? = 0 ]] && echo "[Info] jq has installed, the version is:"
+  jq --version
+fi
+
+echo
+version=20.10.14-3
+type -a docker 2>/dev/null 1>&2
+if [[ $? = 0 ]]; then
+  echo "[Info] docker-ce exists, the version is:"
+  docker -v
+else
+  echo "[Info] docker-ce doesn't exist, install it now..."
+  yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+  curl -o /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
+  yum -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  if [[ $? = 0 ]]; then
+    echo "[Info] docker-ce has been installed, the version is:"
+    docker -v
+    echo "[Info] start docker now"
+    mkdir -p /etc/docker/
+    echo '{"bip":"10.0.254.1/20"}' | tee -a /etc/docker/daemon.json
+    systemctl enable --now docker
+  else
+    echo "[Error] docker-ce install failed, exit"
+    exit 1
+  fi
+fi
+
+echo
+version=1.29.2
+type -a docker-compose 2>/dev/null 1>&2
+if [[ $? = 0 ]]; then
+  echo "[Info] docker-compose exists, the version is:"
+  docker-compose -v
+else
+  echo "[Info] docker-compose doesn't exist, install it now..."
+  curl -SL https://github.com/docker/compose/releases/download/${version}/docker-compose-Linux-x86_64 -o /usr/bin/docker-compose
+  chmod +x /usr/bin/docker-compose
+fi
+
+echo
 type -a node 2>/dev/null 1>&2
 if [[ $? = 0 ]]; then
   echo "[Info] node.js exists, the version is:"
@@ -64,19 +121,21 @@ fi
 
 if [[ -d "${GOPL_DIR_TMP}/gopl-zh.github.com" ]]; then
     cd ${GOPL_DIR_TMP}/gopl-zh.github.com && git pull
-else 
+else
     git clone ${GOPL_GIT} ${GOPL_DIR_TMP}/gopl-zh.github.com
 fi
 
+echo
 if [[ -d "${GOPL_DIR_TMP}/gopl-zh.github.com" ]]; then
   cd ${GOPL_DIR_TMP}/gopl-zh.github.com
   gitbook install
   [[ $? != 0 ]] && echo "[Error] gitbook plugins for gopl-zh install failed." && exit 1
   #yum -y groupinstall "Development Tools"
-  yum -y install gcc
+  yum -y install gcc automake autoconf libtool make
   make
   [[ $? != 0 ]] && echo "[Error] make gopl-zh failed." && exit 1
   rm -rf ${GOPL_DIR}
-  mv ${GOPL_DIR_TMP}/gopl-zh.github.com/_book ${GOPL_DIR}
+  mkdir -p ${GOPL_DIR}
+  mv ${GOPL_DIR_TMP}/gopl-zh.github.com/_book ${GOPL_DIR}/gopl-zh
 fi
 
